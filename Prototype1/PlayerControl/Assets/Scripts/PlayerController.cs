e1dc8898190a17b93a75af66ba23e3c6097c2b3a
FredyFazbars1234 <quiambaoe@roverkids.org>
2025-11-24 13:42:20 -0500

    I'm finna crash out.

--- Prototype1/PlayerControl/Assets/Scripts/PlayerController.cs
+++ Prototype1/PlayerControl/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    public float speed = 5.0f;
+   public float turnSpeed;
+   public float horizontalInput; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +14,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         // Move the vehicle forward.
-        transform.Translate(Vector3.forward*Time.deltaTime* speed);
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
